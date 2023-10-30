b3676da84f519d8e0ed36e779bc21a522b849830
Akash Singh <akashsinghthakur60@gmail.com>
2023-10-30 14:26:59 +0530

    update

---
+++ Variable persistence/provider.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    newrelic = {
+      source = "newrelic/newrelic"
+      version = "3.27.3"
+    }
+  }
+}
+
+provider "newrelic" {
+  # Configuration options
+  api_key="NRAK-5IWGBDCV9NJF0QHEQGOX0MC2ECG"
+  account_id=4038258
+}
